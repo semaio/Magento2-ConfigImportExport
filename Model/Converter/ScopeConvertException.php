0db89cf32d0ce393998df493b438349adb9c3768
Tom Klingenberg <t.klingenberg@netz98.de>
2016-11-14 18:46:30 +0100

    Fix unresolveable codes.
    Resolve codes as codes, not IDs. Looks like a new Magento 2 version does
    not resolve codes "via IDs" any longer.

---
+++ Model/Converter/ScopeConvertException.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * @author Tom Klingenberg <t.klingenberg@netz98.de>
+ * @copyright Copyright (c) 2016 netz98 new media GmbH (http://www.netz98.de)
+ *
+ * @see PROJECT_LICENSE.txt
+ */
+
+namespace Semaio\ConfigImportExport\Model\Converter;
+
+
+use RuntimeException;
+
+class ScopeConvertException extends RuntimeException
+{
+
+}
